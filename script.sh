cbbc077f90ffd628b6778e531ca0cf9673bb4c93
GANESH KALNE <66562863+kalneganesh@users.noreply.github.com>
2022-12-23 17:26:54 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,17 @@
+sudo yum -y install python3-pip
+git clone https://github.com/Digvijay1133/Fuel-Consumption-python.git
+cd Fuel-Consumption-python
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
+
+
+git clone https://github.com/Digvijay1133/agri-python.git
+cd agri-python
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
+
+
+git clone https://github.com/Digvijay1133/USA-House-Pricing-python.git
+cd USA-House-Pricing-python
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
